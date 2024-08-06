52e04ffb003d52278906808822ef01f519e96a4c
ephrem <ephremmamo555@gmail.com>
2024-08-06 14:29:35 +0300

    setup the router

--- router/router.go
+++ router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func SetUpRouter() {
+	r := gin.Default()
+	r.GET("/tasks")
+	r.GET("tasks/:id")
+	r.PUT("/tasks/:id")
+	r.DELETE("/tasks/:id")
+	r.POST("/tasks")
+
+}
